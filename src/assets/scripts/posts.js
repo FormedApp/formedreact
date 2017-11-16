c419225a677d2efac10f58a73428a2c3ba7db858
justin leggett <justin@codingrookie.com>
2017-11-16 15:30:41 -0600

    working on displaying posts

--- src/assets/scripts/posts.js
+++ src/assets/scripts/posts.js
@@ -1,57 +1,61 @@
-// posts
-module.exports = {  
-  post1 : {
-    user_id : 'JessicaDustin',
-    activity_id : 'activity2',
-    private : false,
-    type : 'response',
-    value : 'Prayer Goals: 1.) I would like God to transform my relationship with my parents. 2.) I would like God to enter in to some of my friendships and help me know how to better love my friends and share my faith with them.',
+export const posts = {
+  post1: {
+    user_id: "JessicaDustin",
+    activity_id: "activity2",
+    private: false,
+    type: "response",
+    value:
+      "Prayer Goals: 1.) I would like God to transform my relationship with my parents. 2.) I would like God to enter in to some of my friendships and help me know how to better love my friends and share my faith with them.",
     activity_name: "From Death to Life"
   },
-  
-  post2 : {
-    user_id : 'Sammie',
-    activity_id : 'activity1',
-    private : false,
-    type : 'response',
-    value : 'Dear God I pray that you will help transform my attitude. I need help forgiving people who have wronged me and not holding onto grudges. Amen.',
+
+  post2: {
+    user_id: "Sammie",
+    activity_id: "activity1",
+    private: false,
+    type: "response",
+    value:
+      "Dear God I pray that you will help transform my attitude. I need help forgiving people who have wronged me and not holding onto grudges. Amen.",
     activity_name: "From Old to New"
   },
 
-  post3 : {
-    user_id : 'Jill',
-    activity_id : 'activity1',
-    private : true,
-    type : 'response',
-    value : 'My prayer goals for the next few weeks are: 1. That God would help me to create new habits for prayer and Bible reading. I would like to transform into being more committed and consistent but have always been unsuccessful setting this goal in the past. I need God to help me transform my habits in this way. 2. That God would help me be more diligent in my schoolwork. I know that he wants me to do my best in the things he has given me to do, and I am not giving my best efforts in some areas. I feel convicted about this. 3. That God would help me to remember him as I am living my life throughout the week. It is really easy for me to just forget about him, but I want to be able to see how he is always with me and guiding me in small and large things.',
+  post3: {
+    user_id: "Jill",
+    activity_id: "activity1",
+    private: true,
+    type: "response",
+    value:
+      "My prayer goals for the next few weeks are: 1. That God would help me to create new habits for prayer and Bible reading. I would like to transform into being more committed and consistent but have always been unsuccessful setting this goal in the past. I need God to help me transform my habits in this way. 2. That God would help me be more diligent in my schoolwork. I know that he wants me to do my best in the things he has given me to do, and I am not giving my best efforts in some areas. I feel convicted about this. 3. That God would help me to remember him as I am living my life throughout the week. It is really easy for me to just forget about him, but I want to be able to see how he is always with me and guiding me in small and large things.",
     activity_name: "From Old to New"
   },
 
-  post4 : {
-    user_id : 'Jessica',
-    activity_id : 'activity1',
-    private : false,
-    type : 'reflection',
-    value : '1. Trees in wintertime die and then come back to life each spring. 2. Friendships that are broken can be mended. 3. People who are at war with one another can come to understanding and find peace.',
-    activity_name: "From Old to New"
-
-  },
-
-  post5 : {
-    user_id : 'Sammie',
-    activity_id : 'activity1',
-    private : false,
-    type : 'reflection',
-    value : '1. God transforms hearts to love him  2. God transforms situations that seem hopeless into situations where good comes out of them.  3. Sick people can be healed. ',
+  post4: {
+    user_id: "Jessica",
+    activity_id: "activity1",
+    private: false,
+    type: "reflection",
+    value:
+      "1. Trees in wintertime die and then come back to life each spring. 2. Friendships that are broken can be mended. 3. People who are at war with one another can come to understanding and find peace.",
     activity_name: "From Old to New"
   },
 
-  post6 : {
-    user_id : 'Jill',
-    activity_id : 'activity1',
-    private : false,
-    type : 'reflection',
-    value : '1. I have seen God transform people who have no desire to know him become people whose lives are wholly surrendered to him. 2. I have seen examples of people born into poverty and difficulty become people who overcome the odds and make a real difference in the world. 3. I have seen God make angry people into gentle people. 4. I have seen God transform me from spiritually dry and unbelieving into spiritually thirsty and seeking.',
+  post5: {
+    user_id: "Sammie",
+    activity_id: "activity1",
+    private: false,
+    type: "reflection",
+    value:
+      "1. God transforms hearts to love him  2. God transforms situations that seem hopeless into situations where good comes out of them.  3. Sick people can be healed. ",
+    activity_name: "From Old to New"
+  },
+
+  post6: {
+    user_id: "Jill",
+    activity_id: "activity1",
+    private: false,
+    type: "reflection",
+    value:
+      "1. I have seen God transform people who have no desire to know him become people whose lives are wholly surrendered to him. 2. I have seen examples of people born into poverty and difficulty become people who overcome the odds and make a real difference in the world. 3. I have seen God make angry people into gentle people. 4. I have seen God transform me from spiritually dry and unbelieving into spiritually thirsty and seeking.",
     activity_name: "From Old to New"
   }
 };
--- src/ts/community/ui/community.component.tsx
+++ src/ts/community/ui/community.component.tsx
@@ -1,17 +1,28 @@
+import axios from "axios";
 import * as React from "react";
+import { posts } from "../../../assets/scripts/posts.js";
 import NavBar from "../../common/ui/nav.bar.component";
+
+console.log(posts);
+
 class Community extends React.Component<any, {}> {
   private entry: string;
   constructor(props: any) {
     super(props);
+    this.state = { userPosts: posts };
   }
+  // tslint:disable-next-line:arrow-parens
+  public displayPosts = key => {
+    console.log(posts[key]);
+    // tslint:disable-next-line:semicolon
+  };
 
   public render() {
-    // console.log(this.props);
     return (
       <div>
         <NavBar />
-        Community Page
+        Display Posts
+        {Object.keys(posts).map(this.displayPosts.bind(this))}
       </div>
     );
   }
